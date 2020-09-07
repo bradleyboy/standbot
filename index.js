7fe595d5069fb3de80da25589a4753d549019637
Brad Daily <bdaily@rsglab.com>
2020-09-07 18:59:34 -0400

    user migration support for enterprise grid

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import fs from 'fs';
+
 import client from './lib/client';
 import parser from './lib/parser';
-import { Room, Schedule, Standup } from './lib/db';
+import { User, Room, Schedule, Standup } from './lib/db';
 
 import { INTERVAL } from './constants';
 
@@ -12,51 +14,86 @@ import {
   setChannelActiveStatus,
 } from './actions';
 
-if (process.env.DEBUG) {
-  process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
+  if (process.env.DEBUG) {
     throw err;
-  });
+  } else {
+    console.log('unhandledRejection:', err);
+  }
+});
+
+let intervalId;
+
+async function migrateUsers() {
+  const data = JSON.parse(
+    fs.readFileSync(process.env.USER_MIGRATIONS_FILE, 'utf8')
+  );
+
+  let total = 0;
+  let migrated = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const { oldId, newId } = data[i];
+    const user = await User.findOne({ where: { userId: oldId } });
+    if (user) {
+      migrated++;
+      user.userId = newId;
+      await user.save();
+    }
+
+    total++;
+  }
+
+  console.log(`User migration complete: ${migrated} migrated out of ${total}`);
 }
 
-client.joined(channel => {
-  setChannelActiveStatus(channel.id, true);
-});
-
-client.left(channelId => {
-  setChannelActiveStatus(channelId, false);
-});
-
-client.listen(parser);
-
-const intervalId = setInterval(async () => {
-  const schedules = await Schedule.scope('shouldStart').findAll({
-    limit: 10,
+function start() {
+  client.joined((channel) => {
+    setChannelActiveStatus(channel.id, true);
   });
 
-  schedules.forEach(startScheduledStandup);
-
-  const nagWarn = await Standup.scope('shouldWarn').findAll({
-    include: [Room],
+  client.left((channelId) => {
+    setChannelActiveStatus(channelId, false);
   });
 
-  nagWarn.forEach(warnStandup);
+  client.listen(parser);
 
-  const nagThreat = await Standup.scope('shouldThreat').findAll({
-    include: [Room],
-  });
+  intervalId = setInterval(async () => {
+    const schedules = await Schedule.scope('shouldStart').findAll({
+      limit: 10,
+    });
 
-  nagThreat.forEach(threatenStandup);
+    schedules.forEach(startScheduledStandup);
 
-  const standups = await Standup.scope('shouldClose').findAll({
-    include: [Room],
-    limit: 1,
-  });
+    const nagWarn = await Standup.scope('shouldWarn').findAll({
+      include: [Room],
+    });
 
-  standups.forEach(closeStandup);
-}, INTERVAL);
+    nagWarn.forEach(warnStandup);
+
+    const nagThreat = await Standup.scope('shouldThreat').findAll({
+      include: [Room],
+    });
+
+    nagThreat.forEach(threatenStandup);
+
+    const standups = await Standup.scope('shouldClose').findAll({
+      include: [Room],
+      limit: 1,
+    });
+
+    standups.forEach(closeStandup);
+  }, INTERVAL);
+}
 
 process.on('SIGTERM', () => {
   console.log('SIGTERM received.');
   client.stop();
   clearInterval(intervalId);
 });
+
+if (process.env.USER_MIGRATIONS_FILE) {
+  migrateUsers().then(() => start());
+} else {
+  start();
+}
